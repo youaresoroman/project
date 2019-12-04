2d1b0647a72eca4cf259da51db99833b550593af
youaresoroman <collab@romanjankowski.com>
2019-12-04 20:49:20 +0100

    update

--- profile.js
+++ profile.js
@@ -4,6 +4,6 @@ document.addEventListener("DOMContentLoaded", function(){
     $.get( "https://randomuser.me/api/", function( data ) {
         var user = data['results'][0];
         document.getElementById("my_name").innerHTML = user.name.first + ' ' + user.name.last;
-
+        document.getElementById("social").innerHTML = '<a href="https://www.facebook.com/' + user.login.username + '" class="fa fa-facebook"></a>\n<a href="https://twitter.com/' + user.login.username + '" class="fa fa-twitter"></a>'
 	}, "json" );
 });
