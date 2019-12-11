2b3dba1b4bdb66fb5387c9b2cf44a17ccd4bbea4
youaresoroman <collab@romanjankowski.com>
2019-12-11 08:51:40 -0600

    upd

--- source/functions/useragent.js
+++ source/functions/useragent.js
@@ -13,6 +13,62 @@ For functions leave console log, especially for error handling
 
 document.addEventListener("DOMContentLoaded", function(){
     $.get( "https://yankowski.eu/api/v0/ua", function( data ) {
-        document.getElementById("software").innerHTML = "<p>" + data.software + " version " + data.software_value + "</p>Your platform is " + data.os_name; // + ' ' + data.os_name_value;
+        var software = data.software;
+        var version = data.software_value;
+        var os = data.os_name;
+        var output = '';
+        /*
+            <i class='' style='font-size:36px'></i>
+            
+            
+            fa fa-mobile
+        */
+
+        if (software == 'Opera'){
+            output = output + "<i class='fab fa-opera' style='font-size:36px'></i>";
+        }
+        else if (software == 'Mozilla Firefox') {
+            output = output + "<i class='fa fa-firefox' style='font-size:36px'></i>";
+        }
+        else if (software == 'Google Chrome') {
+            output = output + "<i class='fab fa-chrome' style='font-size:36px'></i>";
+        }
+        else if (software == 'Safari') {
+            output = output + "<i class='fab fa-safari' style='font-size:36px'></i>";
+        }
+        else if (software == 'Mobile Safari') {
+            output = output + "<i class='fab fa-safari' style='font-size:36px'></i>";
+        }
+        else if (software == 'Internet Explorer') {
+            output = output + "<i class='fab fa-edge' style='font-size:36px'></i>";
+        }
+
+        output = output + software + " version " + version;
+
+        output = output + " and your platform is " + os;
+
+        if (os == 'Linux'){
+            output = output + "<i class='fab fa-linux' style='font-size:36px'></i>";
+        }
+        else if (os == 'Android') {
+            output = output + "<i class='fab fa-android' style='font-size:36px'></i>";
+        }
+        else if (os == 'IOS') {
+            output = output + "<i class='fab fa-apple' style='font-size:36px'></i>";
+        }
+        else if (os == 'iPhone OS') {
+            output = output + "<i class='fab fa-apple' style='font-size:36px'></i>";
+        }
+        else if (os == 'Mac OS X') {
+            output = output + "<i class='fab fa-apple' style='font-size:36px'></i>";
+        }
+        else if (os == 'Windows NT') {
+            output = output + "<i class='fab fa-windows' style='font-size:36px'></i>";
+        }
+        else if (os == 'Windows Phone') {
+            output = output + "<i class='fab fa-windows' style='font-size:36px'></i>";
+        }
+
+        document.getElementById("software").innerHTML = output; 
     }, "json" );
 });
